e538747d8859e5993a1d9cd76e934ad449742228
Basanta Goswami <@basantagoswami>
2022-04-15 13:37:44 +0530

    Return matching events on REQ (tags in filters are ignored for now)

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -6,38 +6,55 @@ import {
 import { EventsService } from './events.service';
 import { ErrorMessage } from 'src/utils/errors.util';
 import { MessageType } from './enums/message-type.enum';
+import { SharedService } from '../shared/shared.service';
 
 @WebSocketGateway()
 export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
-  constructor(private eventsService: EventsService) {}
+  constructor(
+    private eventsService: EventsService,
+    private sharedService: SharedService,
+  ) {}
 
   handleConnection(client: any, ...args: any[]) {
-    client.on('message', (data) => {
+    client.on('message', async (data) => {
       let message;
+      // Try to parse JSON, send error message if message isn't JSON
       try {
         message = JSON.parse(`${data}`);
-
-        // EVENT
-        if (message[0] == MessageType.EVENT) {
-          this.eventsService.handleEvent(message[1]);
-        }
-        // REQ
-        if (message[0] == MessageType.REQ) {
-          const subscriptionId = message[1];
-          const filters = [];
-          for (let i = 2; i < message.length; i++) {
-            filters.push(message[i]);
-          }
-          this.eventsService.handleRequest(subscriptionId, filters);
-        }
-        // CLOSE
-        if (message[0] == MessageType.CLOSE) {
-          this.eventsService.handleClose(message[1]);
-        }
       } catch (error) {
         client.send(
-          `["NOTICE", ${ErrorMessage.INVALID_JSON} ${error.message}]`,
+          this.sharedService.formatNotice(
+            `${ErrorMessage.INVALID_JSON} ${error.message}`,
+          ),
         );
+      }
+
+      // If message is JSON, and got successfully parsed
+      if (message != undefined) {
+        const messageType = message[0];
+        switch (messageType) {
+          // EVENT
+          case MessageType.EVENT:
+            this.eventsService.handleEvent(message[1]);
+            break;
+          // REQ
+          case MessageType.REQ:
+            const [, subscriptionId] = message.splice(0, 2);
+            const events = await this.eventsService.handleRequest(
+              subscriptionId,
+              message,
+            );
+            events.forEach((event) => {
+              event.tags = JSON.parse(event.tags);
+              console.log(event.tags);
+              client.send(
+                this.sharedService.formatEvent(subscriptionId, event),
+              );
+            });
+            break;
+          case MessageType.CLOSE:
+            this.eventsService.handleClose(message[1]);
+        }
       }
     });
   }
--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { SharedService } from 'src/shared/shared.service';
-import { Repository } from 'typeorm';
+import { Between, In, LessThan, MoreThan, Repository } from 'typeorm';
 import { Event } from './entities/events.entity';
 
 @Injectable()
@@ -12,22 +12,72 @@ export class EventsService {
     private sharedService: SharedService,
   ) {}
 
+  /**
+   * Save events to database if event is valid
+   * @todo: check if events matches any subscription
+   */
   async handleEvent(event: Event) {
     if (await this.sharedService.validateEvent(event)) {
-      this.eventsRepo.insert(event);
+      event.tags = JSON.stringify(event.tags);
+      this.eventsRepo.save(event);
     }
-    console.log();
   }
 
-  handleRequest(subscriptionId: string, filters: any[]) {
-    console.log(`SUBSCRIPTION ID: ${subscriptionId}`);
-    console.log(`FILTERS:`);
-    filters.forEach((filter) => {
-      console.log(filter);
+  /**
+   * Return events based on the request filter
+   * @todo: push subscription to a subscription object
+   */
+  async handleRequest(subscriptionId: string, filters: any[]) {
+    return this.eventsRepo.find({
+      where: this.createWhereClause(filters),
     });
   }
 
   handleClose(subscriptionId: string) {
     console.log(subscriptionId);
+  }
+
+  /**
+   * Create the where clause that will fetch the appropriate events
+   * @todo: find a more elegant solution ffs
+   * @todo: also make the tags work, they are totally ignored for now
+   */
+  createWhereClause(filters: any[]) {
+    const where = [];
+
+    // For each filter create a where clause, which works as OR
+    for (let i = 0; i < filters.length; i++) {
+      const wherePerFilter = {};
+
+      const { ids, kinds, e, p, since, until, authors } = filters[i];
+
+      if (ids != undefined) {
+        Object.assign(wherePerFilter, { id: In(ids) });
+      }
+      if (kinds != undefined) {
+        Object.assign(wherePerFilter, { kind: In(kinds) });
+      }
+      // if (e != undefined) {
+      // Object.assign(wherePerFilter, { tags.e: In(e) });
+      // }
+      // if (p != undefined) {
+      // Object.assign(wherePerFilter, { tags.p In(p) })
+      // }
+      if (since != undefined) {
+        Object.assign(wherePerFilter, { created_at: MoreThan(since) });
+      }
+      if (until != undefined) {
+        Object.assign(wherePerFilter, { created_at: LessThan(since) });
+      }
+      if (since != undefined) {
+        Object.assign(wherePerFilter, { created_at: Between(since, until) });
+      }
+      if (authors != undefined) {
+        Object.assign(wherePerFilter, { pubkey: In(authors) });
+      }
+
+      where.push(wherePerFilter);
+    }
+    return where;
   }
 }
